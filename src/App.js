5117488b67d0d860e6761413118810e797886636
aisha <68122062+aisha000@users.noreply.github.com>
2024-05-06 21:49:16 +0300

    d

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import Header from "./components/Header";
 import Balance from "./components/Balance";
 import IncomeExpenses from "./components/IncomeExpenses";
 import TransactionList from "./components/TransactionList";
+import AddTransaction from "./components/AddTransaction";
+// import AppReducer from "./context/AppReducer";
+import { GlobaProvider } from "./context/GlobalState";
 
 function App() {
   return (
-    <>
+      <GlobaProvider >
       <Header />
       <div className="container">
         <Balance />
       </div>
       <IncomeExpenses />
       <TransactionList />
-    </>
+      <AddTransaction />
+      </GlobaProvider>
   );
 }
 
