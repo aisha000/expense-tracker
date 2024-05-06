b9b5c0c99a14552cf6a5e5025c43b34bb8622a3b
aisha <ayshama2030@gmail.com>
2024-05-06 14:40:46 +0300

    expenses

---
+++ src/components/TransactionList.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TransactionList = () => {
+  return (
+    <>
+      <h3>History</h3>
+      <ul className="list">
+        {/* {transactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))} */}
+      </ul>
+    </>
+  );
+};
+
+export default TransactionList;
